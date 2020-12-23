883d24235fff0c882e70dd2eb51a82c25f8f73ba
Martin Suesskraut <22873554+msuesskraut@users.noreply.github.com>
2020-12-23 22:17:50 +0100

    added Operand helpers methods

--- src/ast.rs
+++ src/ast.rs
@@ -7,6 +7,29 @@ pub enum Operand {
     Term(Box<Term>),
 }
 
+impl Operand {
+    pub fn is_num(&self) -> bool {
+        match self {
+            Operand::Number(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_sym(&self) -> bool {
+        match self {
+            Operand::Symbol(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_symbol(&self, sym: &str) -> bool {
+        match self {
+            Operand::Symbol(s) if s == sym => true,
+            _ => false,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Operation {
     Add,
@@ -39,4 +62,50 @@ pub struct Assignment {
 pub enum Statement {
     Expression(Expression),
     Assignment(Assignment),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn operand_is_num() {
+        assert!(Operand::Number(1.0).is_num());
+    }
+
+    fn create_term() -> Term {
+        let lhs = Operand::Number(1.0);
+        let rhs = Operand::Number(1.0);
+        let op = Operation::Add;
+        Term { op, lhs, rhs }
+    }
+
+    #[test]
+    fn operand_is_not_num() {
+        assert!(!Operand::Symbol("x".to_string()).is_num());
+        assert!(!Operand::Term(Box::new(create_term())).is_num());
+    }
+
+    #[test]
+    fn operand_is_sym() {
+        assert!(Operand::Symbol("x".to_string()).is_sym());
+    }
+
+    #[test]
+    fn operand_is_not_sym() {
+        assert!(!Operand::Number(1.0).is_sym());
+        assert!(!Operand::Term(Box::new(create_term())).is_sym());
+    }
+
+    #[test]
+    fn operand_is_symbol() {
+        assert!(Operand::Symbol("x".to_string()).is_symbol("x"));
+    }
+
+    #[test]
+    fn operand_is_not_symbol() {
+        assert!(!Operand::Symbol("y".to_string()).is_symbol("x"));
+        assert!(!Operand::Number(1.0).is_sym());
+        assert!(!Operand::Term(Box::new(create_term())).is_sym());
+    }
 }
